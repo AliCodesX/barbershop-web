0018e49db45d606b50cdcc9c32c38200f8d41ca5
Ali <alizadeh1412@gmail.com>
2024-11-09 22:34:14 +0100

    main- Navbar- Componente erstellt und eingefügt

--- barbershop/.next/static/chunks/src_app_layout_tsx_36ac26._.js
+++ barbershop/.next/static/chunks/src_app_layout_tsx_36ac26._.js
@@ -5,8 +5,8 @@
 (globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
   "path": "static/chunks/src_app_layout_tsx_36ac26._.js",
   "chunks": [
-    "static/chunks/src_app_c7d265._.css",
-    "static/chunks/src_app_layout_tsx_20a9fc._.js"
+    "static/chunks/[root of the server]__2f4ab0._.css",
+    "static/chunks/_0447fb._.js"
   ],
   "source": "dynamic"
 });
